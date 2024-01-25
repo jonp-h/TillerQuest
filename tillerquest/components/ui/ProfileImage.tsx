8a59088300aadd1ab5755d98829b6d639c2db8dd
Jon <128969752+jonp-h@users.noreply.github.com>
2024-01-25 21:58:43 +0100

    Update ProfileImage component styling and image

--- tillerquest/components/ui/ProfileImage.tsx
+++ tillerquest/components/ui/ProfileImage.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function ProfileImage() {
   return (
-    <div className="bg-slate-800 md:mt-24 p-4 rounded-full">
+    <div className=" from-slate-700 to-slate-800 bg-gradient-radial md:mt-24 p-4 rounded-full">
       <div className="">
         <Image
           className="rounded-full"
           draggable="false"
-          src="/classes/rogue3.jpg"
+          src="/classes/cleric1.jpg"
           alt="Tiller Quest logo"
           width={340}
           height={150}
