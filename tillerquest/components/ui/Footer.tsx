b79fbe14b0231c58c18ff00eaa012c0d4317ede9
Jon <128969752+jonp-h@users.noreply.github.com>
2024-01-25 21:53:40 +0100

    Update Footer component

--- tillerquest/components/ui/Footer.tsx
+++ tillerquest/components/ui/Footer.tsx
@@ -5,12 +5,12 @@ export default function NavBar() {
   return (
     <div className="flex flex-col w-full py-10 items-center bg-gradient-to-r from-slate-900 to-slate-950">
       <h1>
-        Made with love by{" "}
-        <Link href="#" className="text-blue-400">
-          Pape
-        </Link>
+        Made with ❤️ by {}
+        {/* <Link href="#" className="text-blue-400"> */}
+        Pape
+        {/* </Link> */}
       </h1>
-      <h1>2023</h1>
+      <h1>2024</h1>
 
       {/* <Link href="/login">
         <p>Login</p>
