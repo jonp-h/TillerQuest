f4ce48781b5f30a58b5f60480aaca4fe91081c36
Jon <128969752+jonp-h@users.noreply.github.com>
2024-02-04 00:20:12 +0100

    Add Label and InputWithLabel components

---
+++ tillerquest/components/ui/InputWithLabel.tsx
@@ -0,0 +1,31 @@
+import { Input } from "@/components/ui/Input";
+import { Label } from "@/components/ui/label";
+
+export function InputWithLabel(props: {
+  type: string;
+  text: string;
+  id: string;
+  placeholder: string;
+  value?: string;
+  required?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}) {
+  return (
+    <div className="flex flex-col w-full max-w-sm gap-1.5">
+      <Label
+        htmlFor={props.id}
+        className="text-sm text-left ml-0.5 leading-none"
+      >
+        {props.text}
+      </Label>
+      <Input
+        type={props.type}
+        id={props.id}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        value={props.value}
+        required={props.required}
+      />
+    </div>
+  );
+}
