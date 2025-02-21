815770aee337b068bc0c82e2c429732b75ae2715
Jon P. Hallem <128969752+jonp-h@users.noreply.github.com>
2025-02-21 09:30:58 +0100

    Add user special status management component and update user role functionality

--- frontend/data/admin/adminUserInteractions.ts
+++ frontend/data/admin/adminUserInteractions.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth";
 import { db } from "@/lib/db";
+import { UserRole } from "@prisma/client";
 
 export const getAllUsers = async () => {
   const session = await auth();
@@ -39,4 +40,60 @@ export const getAllDeadUsers = async () => {
     },
   });
   return deadUsers;
+};
+
+export const getUsersSpecialStatus = async () => {
+  const session = await auth();
+  if (session?.user.role !== "ADMIN") {
+    return null;
+  }
+
+  const users = await db.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      lastname: true,
+      username: true,
+      special: true,
+      role: true,
+    },
+  });
+  return users;
+};
+
+export const updateUserSpecialStatus = async (
+  userId: string,
+  special: string[],
+) => {
+  const session = await auth();
+  if (session?.user.role !== "ADMIN") {
+    throw new Error("Not authorized");
+  }
+
+  const user = await db.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      special,
+    },
+  });
+  return user;
+};
+
+export const changeUserRole = async (userId: string, role: string) => {
+  const session = await auth();
+  if (session?.user.role !== "ADMIN") {
+    throw new Error("Not authorized");
+  }
+
+  const user = await db.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      role: role as UserRole,
+    },
+  });
+  return user;
 };
