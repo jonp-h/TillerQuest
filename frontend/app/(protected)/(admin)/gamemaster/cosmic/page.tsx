d60eaf5b7c014df6aacae0e6a3ae6c6d75950a97
Jon <128969752+jonp-h@users.noreply.github.com>
2025-03-07 13:58:18 +0100

    style: format code and improve consistency in configuration and component files

--- frontend/app/(protected)/(admin)/gamemaster/cosmic/page.tsx
+++ frontend/app/(protected)/(admin)/gamemaster/cosmic/page.tsx
@@ -1,16 +1,6 @@
 import MainContainer from "@/components/MainContainer";
 import { getAllCosmicEvents } from "@/data/admin/cosmic";
-import {
-  Button,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from "@mui/material";
+import { Typography } from "@mui/material";
 import React from "react";
 import RerollCosmic from "./_components/RerollCosmic";
 
