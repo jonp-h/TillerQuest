820d1a330ad5621332f722ff4680bace50965ad9
Enderz <40892919+Enderz420@users.noreply.github.com>
2025-05-22 13:23:56 +0200

    feat: :sparkles: add new enemy
    add new enemy types for a wider variety

--- backend/db/enemies.js
+++ backend/db/enemies.js
@@ -17,5 +17,32 @@ const enemies = [
     gold: 300,
     xp: 400,
   },
+  {
+    id: 3,
+    name: "Cultist",
+    maxHealth: 150,
+    attack: "1d6",
+    icon: "/dungeons/slug.png", // temporary icon
+    gold: 400,
+    xp: 350,
+  },
+  {
+    id: 4,
+    name: "Hyena",
+    maxHealth: 125,
+    attack: "1d8",
+    icon: "/dungeons/slug.png", // temporary icon
+    gold: 500,
+    xp: 400,
+  },
+  {
+    id: 5,
+    name: "Dragon",
+    maxHealth: 400,
+    attack: "1d8",
+    icon: "/dungeons/slug.png", // temporary icon
+    gold: 1000,
+    xp: 600,
+  },
 ];
 export default enemies;
