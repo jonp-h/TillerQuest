39e39dca637493b8040a72aa9c9c7a2631a07a46
Jon Pape Hallem <128969752+jonp-h@users.noreply.github.com>
2025-12-29 10:32:31 +0100

    feat: move prisma schema to monorepo root

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -4,7 +4,16 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db =
+  globalThis.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
 
-//makes sure we don't create a new connection if one already exists (eg. when hot reloading)
+//makes sure we don't create a new connection if one already exists
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+export * from "@prisma/client";
